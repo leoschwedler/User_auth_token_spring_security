7c2da75978f23f677b9b4249f0ce672d72f4fbd2
leonardo schwuedller <77402907+leoschwedler@users.noreply.github.com>
2025-09-18 19:46:27 -0300

    first commit

---
+++ src/test/java/auth_token/AuthTokenApplicationTests.java
@@ -0,0 +1,13 @@
+package auth_token;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthTokenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
